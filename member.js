d3d2f9e4f82ad1ef82f3d45d84f1e0a40b22db66
c-tateno <56812089+c-tateno@users.noreply.github.com>
2024-07-01 08:40:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "Sunny";
+    member.age = 18;
+    member.skills = ["Javascript", "React", "Node"];
+    return member;
+}
